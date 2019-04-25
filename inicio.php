c5030a951d9d0a7f4cf55abddb5341d6bd410932
josecaos <jazzvoon@gmail.com>
2019-04-24 21:17:23 -0500

    Debugging

--- inicio.php
+++ inicio.php
@@ -10,7 +10,8 @@ get_header('inicio');
 get_template_part('secciones/home/heroscreen');
 get_template_part('secciones/home/nosotros');
 get_template_part('secciones/home/label');
-get_template_part('secciones/home/rotoblog');
+// get_template_part('secciones/home/rotoblog');
+get_template_part('secciones/home/fb_feed');
 get_template_part('secciones/home/rotoradio');
 // get_template_part('secciones/home/mercado');
 
---
+++ secciones/home/fb_feed.php
@@ -0,0 +1,63 @@
+<section id="inicio-feed" class="grid-x cell columns small-12 large-6 h-100-v p-top rel color-terciario-0-bg color-blanco end">
+
+  <a href="<?php echo get_the_permalink(7);?>" class="columns p-0 color-negro-hover">
+    <h1 class="titulo-inicio-anim h-a columns p-0 text-center">
+      RotoFeed
+    </h1>
+  </a>
+
+  <div class="row columns small-12 align-middle p-0">
+
+    <div id="inicio-blog-slider" class="slider-blog columns p-0 h-75 ovH">
+      <?php
+
+      $args =  array( 'post_type' => 'post', 'showposts' => 6 );
+      $query =  new WP_Query($args);
+
+      if($query->have_posts()):
+        while ($query->have_posts()): $query -> the_post();
+        ?>
+        <article class="columns p-0-1">
+
+          <a href="<?php echo get_the_permalink();?>" class="columns p-0 color-blanco color-verde-roto-hover color-verde-roto-hover-bd text-shadow">
+            <div class="columns small-4 imgLiquid imgLiquidFill">
+              <?php echo get_the_post_thumbnail(); ?>
+            </div>
+
+            <div class="columns small-8 home-blog-data cortina-negro">
+
+              <h3 class="h-a columns p-0 color-blanco">
+                <?php echo get_the_title(); ?>
+              </h3>
+              <!-- <div class="columns h-a color-blanco">
+                <?php // echo excerpt(9,"... leer mas >>"); ?>
+              </div> -->
+              <div class="columns p-0 h-a">
+                <div class="columns p-0 small-12 medium-8 h-a text-left color-blanco">
+                  <?php echo "Escrito por: " . get_the_author_meta( 'display_name' ); ?>
+                </div>
+                <div class="columns p-0 small-12 medium-4 h-a text-right color-blanco">
+                  <?php echo get_the_date(); ?>
+                </div>
+              </div>
+
+            </div>
+
+          </a>
+        </article>
+
+        <?php
+      endwhile;
+    endif;
+    ?>
+
+  </div>
+
+  <!--  -->
+  <div class="columns text-center h-5 p-0">
+    <small class="color-verde-roto"> << drag >> </small>
+  </div>
+  <!--  -->
+</div>
+
+</section>
