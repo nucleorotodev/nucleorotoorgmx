1a509964060a124b50c4eb24af909cee94d2d38e
josecaos <jazzvoon@gmail.com>
2019-04-29 11:18:23 -0500

    Cambio de FB Feed

--- secciones/home/fb_feed.php
+++ secciones/home/fb_feed.php
@@ -7,29 +7,9 @@
   </a>
 
   <div class="row columns small-12 align-middle p-0">
-<!--  -->
-    <!-- <div id="fb-root" class="ha"></div> -->
-    <script async defer crossorigin="anonymous" src="https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v3.2&appId=380481349218741"></script>
-<!--  -->
-    <div class="columns p-0 h-75 ovH">
+    <!--  -->
 
-<center>
-      <!--  -->
-<div class="fb-page" data-href="https://www.facebook.com/nucleorotomusica/"
-data-tabs="timeline"
-data-width=""
-data-height=""
-data-small-header="false"
-data-adapt-container-width="true"
-data-hide-cover="true"
-data-show-facepile="false">
-<blockquote cite="https://www.facebook.com/nucleorotomusica/" class="fb-xfbml-parse-ignore"><a href="https://www.facebook.com/nucleorotomusica/">Nucleoroto</a></blockquote>
-</div>
-<!--  -->
-</center>
-
-    </div>
-
+    <?php echo do_shortcode('[custom-facebook-feed]'); ?>    
     <!--  -->
     <div class="columns text-center h-5 p-0">
       <small class="color-verde-roto"> <a href="https://facebook.com/nucleorotomusica" target="_blank">Visita nuestro facebook</a> </small>
