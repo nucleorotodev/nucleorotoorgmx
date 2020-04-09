d1c41d7fecaadfc9f533a26b82457bfcb8b771a5
josecaos <jazzvoon@gmail.com>
2020-04-08 20:39:30 -0500

    Regresa metadato nombre de artista - cancion

--- secciones/radio/station-radio.php
+++ secciones/radio/station-radio.php
@@ -1,8 +1,8 @@
 
-<h4 class="small-12 p-t-2"><small>Servidor: </small>
+<h4 class="small-12 p-t-2"><small>Server: </small>
 
   <?php
-  $online = "Activo, Transmitiendo ..."; // Displays when stream is online
+  $online = "Activo, Transmitiendo"; // Displays when stream is online
   $offline['server'] = "Inactivo!"; // Displays when server is offline
   $offline['source'] = "Activo, En espera ..."; // Displays when server is online with no source
   $ch = curl_init();
@@ -28,39 +28,39 @@
 
 </h4>
 
-<!-- <h4 class="small-12"><small>Track: </small> -->
+<h4 class="small-12"><small>Track: </small>
 
   <?php
-  // $ch = curl_init();
-  // curl_setopt($ch, CURLOPT_URL, "s48.myradiostream.com:8486/7.html");
-  // curl_setopt($ch,CURLOPT_USERAGENT,'Mozilla');
-  // curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-  // curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 5);
-  // $data = curl_exec($ch);
-  // curl_close($ch);
-  // $data = str_replace('</body></html>', "", $data);
-  // $split = explode(',', $data);
-  // if (empty($split[6])) {
-  // 	$title = "The current song is not available ";
-  // } else {
-  // 	$count = count($split);
-  // 	$i = "6";
-  // 	while($i<=$count) {
-  // 		if ($i > 6) {
-  // 			$title .= "," . $split[$i];
-  // 		} else {
-  // 			$title .= $split[$i];
-  // 		}
-  // 		$i++;
-  // 	}
-  // }
-  // $title = substr($title, 0, -1);
-  // echo $title;
+  $ch = curl_init();
+  curl_setopt($ch, CURLOPT_URL, "s48.myradiostream.com:8486/7.html");
+  curl_setopt($ch,CURLOPT_USERAGENT,'Mozilla');
+  curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+  curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 5);
+  $data = curl_exec($ch);
+  curl_close($ch);
+  $data = str_replace('</body></html>', "", $data);
+  $split = explode(',', $data);
+  if (empty($split[6])) {
+  	$title = "Metadato no disponible ";
+  } else {
+  	$count = count($split);
+  	$i = "6";
+  	while($i<=$count) {
+  		if ($i > 6) {
+  			$title .= "," . $split[$i];
+  		} else {
+  			$title .= $split[$i];
+  		}
+  		$i++;
+  	}
+  }
+  $title = substr($title, 0, -1);
+  echo $title;
   ?>
-<!-- </h4> -->
+</h4>
 
 
-<h4 class="small-12"><small>Almas escuchando: </small>
+<h4 class="small-12"><small>Souls: </small>
 <?php
 $ch = curl_init();
 curl_setopt($ch, CURLOPT_URL, "s48.myradiostream.com:8486/7.html");
